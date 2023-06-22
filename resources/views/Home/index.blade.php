3c1837d5c0ed7398f962d4f8b3b6496c08f354d9
dinar wijaksono <dinarwijaksono11@gmail.com>
2023-06-22 21:46:47 +0700

    Refactor: setup halaman Home/index

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>web undangan</title>
+
+    <link rel="stylesheet" href="/asset/tailwind/output.css">
+
+</head>
+
+<body class="bg-color-4 ">
+
+    <nav class="p-2 " style="background-color: #222831; color: #EEEEEE;">
+        <h1><b>Buat undangan</b></h1>
+    </nav>
+
+    <main style="min-height: 900px;">
+
+        <section class="py-20 text-center bg-slate-200">
+
+            <h2 class="font-bold">JASA UNDANGAN DIGITAL</h2>
+            <p>Solusi kebutuhan jasa undangan digital</p>
+
+        </section>
+
+
+        <section class="px-2 py-20 text-center">
+            <h2 class="font-bold underline mb-4">KENAPA HARUS DI BUATUNDANGAN</h2>
+
+            <div class="mb-2 bg-green-300 p-2">
+                <h4 class="font-bold ">HARGA MURAH</h4>
+                <p>Harga mulai Rp 100.000an saja .</p>
+            </div>
+
+            <div class="mb-2 bg-blue-300 p-2">
+                <h4 class="font-bold ">PROSES CEPAT</h4>
+                <p>Proses pengerjaan cepat estimasi 1 - 2 harian jadi.</p>
+            </div>
+
+            <div class="mb-2 bg-blue-300 p-2">
+                <h4 class="font-bold ">UNLIMITED REVISI</h4>
+                <p>Revisi sampai anda puas tanpa batasan.</p>
+            </div>
+
+            <div class="mb-2 bg-blue-300 p-2">
+                <h4 class="font-bold ">MASA EXPIRED LAMA</h4>
+                <p>Undangan akan expired h+7 pada saat acara berakhir, kapanpun anda memesannya.</p>
+            </div>
+
+        </section>
+
+
+        <section class="p-2">
+            <h3 class="mb-2 underline text-center font-bold">DAFTAR PRODUK</h3>
+
+            <div class="bg-white shadow-lg mb-2 p-1">
+                <div class="flex">
+                    <div class="basis-1/3 p-1">
+                        <img src="" alt="gambar" class="-w-full bg-slate-500" style="height: 200px;">
+                    </div>
+
+                    <div class="basis-2/3 p-1 pt-7">
+                        <p class="font-bold mb-4">Tema 1</p>
+                        <p>Dilihat 1000</p>
+                        <p>Dipesan 421</p>
+                        <p>Harga Rp 100.000</p>
+                    </div>
+                </div>
+
+                <div class="flex gap-2 p-1">
+
+                    <a href="" class="basis-1/2 block text-[12px] bg-color-3 hover:bg-color-2 text-white p-1 rounded text-center">Lihat
+                        demo</a>
+                    <a href="" class="basis-1/2 block text-[12px] bg-color-3 hover:bg-color-2 text-white p-1 rounded text-center">Pesan
+                        sekarang</a>
+
+                </div>
+            </div>
+
+        </section>
+
+    </main>
+
+    <footer class="p-2 text-center" style="background-color: #171717; color: #EDEDED;">
+        <p>Build by - dinarwijaksono11@gmail.com</p>
+        <a href="https://wa.me/0859106979837" target="_blank" class="text-blue-400 hover:underline">
+            <p>WA 0859 1069 7983 7</p>
+        </a>
+    </footer>
+
+
+</body>
+
+</html>
